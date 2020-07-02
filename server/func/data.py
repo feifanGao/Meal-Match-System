4cf006500c99d0b6251592ae7b1302a265098e28
Ruixian <z5158318@student.unsw.edu.au>
2020-07-02 21:40:22 +1000

    edited

---
+++ server/func/data.py
@@ -0,0 +1,16 @@
+#sys
+users = {}
+userdata = {
+    'u_id' : None,
+    'user_name' : None,
+    'email' : None,
+    'token': None,
+    'logged' : None,
+}
+#secret for jwt token
+secret = 'secret'
+
+active_tokens = {
+    'token' : []
+}
+
